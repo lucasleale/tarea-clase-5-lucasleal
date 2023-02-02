20bcba4f497244da23c933d3bbddc066fec654a4
lucasleale <e.lucas.leal@gmail.com>
2023-02-02 12:33:23 -0300

    muestra la informacion formateada
    Se agregó función que formatea los datos del usuario.
    Se agregó control de elementos html para mostrar los datos del usuario

--- tarea-2-clase-5/tarea-2-clase-5.js
+++ tarea-2-clase-5/tarea-2-clase-5.js
@@ -6,10 +6,20 @@ mostrarInformacion.onclick = function(){
     const primerNombreUsuario = document.querySelector("#primer-nombre-usuario").value;
     const segundoNombreUsuario = document.querySelector("#segundo-nombre-usuario").value;
     const apellidosUsuario = document.querySelector("#apellidos-usuario").value;
-    const edadUsuario = Number(document.querySelector("#edad-usuario").value);
-    
+    const edadUsuario = Number(document.querySelector("#edad-usuario").value) || "--";
+    const datosUsuario = formatearDatosUsuario(primerNombreUsuario, segundoNombreUsuario, apellidosUsuario, edadUsuario);
+    const saludoUsuario = `Bienvenido, ${primerNombreUsuario} !`;
+    document.querySelector("#encabezado").innerText = saludoUsuario;
+    document.querySelector("#datos-texto").innerText = datosUsuario;
+    console.log(datosUsuario);
     return false;
    
 }
 
+function formatearDatosUsuario(primerNombreUsuario, segundoNombreUsuario, apellidosUsuario, edadUsuario){
+
+    datosUsuarioFormateados = `Nombre: ${primerNombreUsuario} ${segundoNombreUsuario} ${apellidosUsuario} Edad: ${edadUsuario} años`;
+
+    return datosUsuarioFormateados;
+}
 
